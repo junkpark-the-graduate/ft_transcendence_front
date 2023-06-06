2ef8ac19df3dee3563a1f702f0af529f87e340b9
hermin9804 <hermin9804@gmail.com>
2023-06-06 17:32:54 +0900

    feat: 42auth 사용법 해보기
    Co-authored-by: junqwoopark <junqwoopark@users.noreply.github.com>
    Co-authored-by: jrim <jrim42@users.noreply.github.com>
    Co-authored-by: seunghye218 <seunghye218@users.noreply.github.com>
    Co-authored-by: namzisun <namzisun@users.noreply.github.com>

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+export default function Page(params: any, searchParams: any) {
+  console.log(params);
+  console.log(searchParams);
+  return <h1>HelloWorld</h1>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,25 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import { useRouter } from "next/navigation";
+
+const UID =
+  "u-s4t2ud-4155cf0a6c38718ad15878b1a564844536bd776c25746afae43f39eac5ab2b22";
+
+const SECRET =
+  "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";
+
+const REDIRECT_URI = encodeURI("http://127.0.0.1:3000/auth");
+const AUTH_URL = `https://api.intra.42.fr/oauth/authorize?client_id=${UID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 export default function Home() {
+  const router = useRouter();
+  function onClick() {
+    console.log(REDIRECT_URI);
+    router.push(AUTH_URL);
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div>
+      <button onClick={onClick}>sign in with 42 intra</button>
+    </div>
+  );
 }
