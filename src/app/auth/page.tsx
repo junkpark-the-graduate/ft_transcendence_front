68aa53ba27e1baf4bd96aeb70c94d530e889820f
junqwoopark <junqwoopark@gmail.com>
2023-06-07 14:56:59 +0900

    feat: /auth 백엔드 연결 Co-authored-by: 허민 (Her Min) <hermin9804@gmail.com> Co-authored-by: jrim <jrim42@users.noreply.github.com> Co-authored-by: seunghye218 <seunghye218@users.noreply.github.com> Co-authored-by: namzisun <namzisun@users.noreply.github.com>

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,7 +1,30 @@
 "use client";
 
-export default function Page(params: any, searchParams: any) {
-  console.log(params);
-  console.log(searchParams);
-  return <h1>HelloWorld</h1>;
+import { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const [accessToken, setAccessToken] = useState("");
+
+  async function signIn() {
+    const uri: string = `http://localhost:3001/auth?code=${searchParams.code}`;
+    const res: AxiosResponse = await axios.post(uri);
+    setAccessToken(res.data.accessToken);
+  }
+
+  useEffect(() => {
+    signIn();
+  }, []);
+  return (
+    <div>
+      <h1>HelloWorld</h1>
+      <h1>{accessToken}</h1>
+    </div>
+  );
 }
+
+// Page.getInitial
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,14 @@
 
 import { useRouter } from "next/navigation";
 
-const UID =
-  "u-s4t2ud-4155cf0a6c38718ad15878b1a564844536bd776c25746afae43f39eac5ab2b22";
-
-const SECRET =
-  "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";
-
-const REDIRECT_URI = encodeURI("http://127.0.0.1:3000/auth");
-const AUTH_URL = `https://api.intra.42.fr/oauth/authorize?client_id=${UID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+// const REDIRECT_URI = encodeURI("http://127.0.0.1:3000/auth");
+const AUTH_URL = `https://api.intra.42.fr/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_FT_UID}&redirect_uri=${process.env.NEXT_PUBLIC_FT_REDIRECT_URI}&response_type=code`;
 
 export default function Home() {
   const router = useRouter();
+  console.log("env: ", process.env);
   function onClick() {
-    console.log(REDIRECT_URI);
+    console.log(process.env.NEXT_PUBLIC_FT_REDIRECT_URI);
     router.push(AUTH_URL);
   }
   return (
