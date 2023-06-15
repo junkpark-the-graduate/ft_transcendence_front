a17ab08bf52736269d24c159785d97be1b20a13c
junqwoopark <junqwoopark@gmail.com>
2023-06-15 20:28:03 +0900

    feat: User 수정 페이지 form, validate with MUI
    Co-authored-by: namzisun <namzisun@users.noreply.github.com>
    Co-authored-by: jrim <jrim42@users.noreply.github.com>
    Co-authored-by: 허민 (Her Min) <hermin9804@gmail.com>
    Co-authored-by: seunghye218 <seunghye218@users.noreply.github.com>

--- src/app/edit/components/Edit.tsx
+++ src/app/edit/components/Edit.tsx
@@ -1,27 +1,25 @@
 "use client";
 
-import CustomButton from "@/app/user/components/CustomButton";
-import { useForm, FieldErrors } from "react-hook-form";
+import { useState } from "react";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
+import { Switch, TextField } from "@mui/material";
 import Button from "@mui/material/Button";
+import React, { Component } from "react";
 
-interface HookFormTypes {
-  name: string;
-  email: string;
-}
-
-export function getToken() {
-  const tokenCookie = Cookies.get("accessToken");
-  return tokenCookie ? tokenCookie : null;
-}
-
-const HookForm = () => {
-  const { register, handleSubmit, reset, setValue } = useForm<HookFormTypes>();
+const Edit = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [twoFactor, setTwoFactor] = useState(false);
   const router = useRouter();
 
-  const onValid = async (data: HookFormTypes) => {
-    const { name, email } = data;
+  function getToken() {
+    const tokenCookie = Cookies.get("accessToken");
+    return tokenCookie ? tokenCookie : null;
+  }
+
+  async function handleSubmit(event: any) {
+    event.preventDefault();
     const token = getToken();
     const res = await fetch("http://127.0.0.1:3001/user", {
       method: "PATCH",
@@ -29,7 +27,11 @@ const HookForm = () => {
         Authorization: `Bearer ${token}`,
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(data),
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        twoFactor: twoFactor,
+      }),
     });
     if (res.ok) {
       router.push("/user");
@@ -37,60 +39,79 @@ const HookForm = () => {
     } else {
       console.log("Failed to update user");
     }
-  };
-  const onInvalid = (error: FieldErrors) => {
-    console.log(error);
-  };
+  }
 
   return (
-    <form onSubmit={handleSubmit(onValid, onInvalid)}>
-      <ul>
-        <li>
-          <input
-            {...register("name", {
-              required: true,
-              maxLength: {
-                value: 20,
-                message: "name is mandatory",
-              },
-            })}
-            type="text"
-            placeholder="new name"
-          />
-        </li>
-        <li>
-          <input
-            {...register("email", {
-              required: true,
-              maxLength: {
-                value: 250,
-                message: "email is mandatory",
-              },
-            })}
-            type="email"
-            placeholder="new email"
-          />
-        </li>
-        <li>
-          <div>
-            <Button
-              type="submit"
-              variant="contained"
-              sx={{
-                color: "#0b131f",
-                backgroundColor: "#90caf9",
-                fontWeight: "bold",
-              }}
-            >
-              save
-            </Button>
-          </div>
-        </li>
-      </ul>
-    </form>
+    <>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          required
+          inputProps={{ maxLength: 10 }}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          type="name"
+          id="name"
+          name="name"
+          variant="outlined"
+          sx={{
+            input: { color: "white" },
+            "& label.Mui-focused": {
+              "& > fieldset": { borderColor: "white" },
+            },
+            "& .MuiOutlinedInput-root": {
+              "& > fieldset": { borderColor: "white" },
+            },
+            "& .MuiOutlinedInput-root:hover": {
+              "& > fieldset": { borderColor: "white" },
+            },
+          }}
+        />
+        <TextField
+          required
+          inputProps={{ maxLength: 50 }}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+          type="email"
+          id="email"
+          name="email"
+          variant="outlined"
+          sx={{
+            input: { color: "white" },
+            "& label.Mui-focused": {
+              "& > fieldset": { borderColor: "white" },
+            },
+            "& .MuiOutlinedInput-root": {
+              "& > fieldset": { borderColor: "white" },
+            },
+            "& .MuiOutlinedInput-root:hover": {
+              "& > fieldset": { borderColor: "white" },
+            },
+          }}
+        />
+        <Switch
+          checked={twoFactor}
+          onChange={() => {
+            setTwoFactor(!twoFactor);
+          }}
+        ></Switch>
+        <div>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              color: "#0b131f",
+              backgroundColor: "#90caf9",
+              fontWeight: "bold",
+            }}
+          >
+            save
+          </Button>
+        </div>
+      </form>
+    </>
   );
 };
 
-export default function Edit() {
-  return <HookForm />;
-}
+export default Edit;
