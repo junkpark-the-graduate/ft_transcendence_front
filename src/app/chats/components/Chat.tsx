2e6c741ab027483c3d72a8b4e0f21394672f2932
hermin9804 <hermin9804@gmail.com>
2023-06-29 15:14:50 +0900

    feat: 채팅 ui 완성

--- src/app/chats/components/Chat.tsx
+++ src/app/chats/components/Chat.tsx
@@ -1,20 +1,27 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { io, Socket } from "socket.io-client";
-import { Input } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
+import { Input, Button, Text, Box, Flex } from "@chakra-ui/react";
 
 type ChatType = {
+  socketId: string;
   username: string;
-  chat: string;
+  message: string;
 };
 
 const Chat: React.FC = () => {
   const [username, setUsername] = useState<string>("");
-  const [chat, setChat] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
   const [chatList, setChatList] = useState<ChatType[]>([]);
   const [socket, setSocket] = useState<Socket | null>(null);
+  const messagesEndRef = useRef<null | HTMLDivElement>(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(scrollToBottom, [chatList]);
 
   // const ENDPOINT = "http://localhost:4242/chattings";
   const ENDPOINT = "ws://localhost:4242/chattings";
@@ -35,6 +42,7 @@ const Chat: React.FC = () => {
     });
 
     socketIo.on("new_chat", (data: ChatType) => {
+      console.log("data!!!!!", data);
       setChatList((oldChatList) => [...oldChatList, data]);
     });
 
@@ -47,50 +55,87 @@ const Chat: React.FC = () => {
     };
   }, [ENDPOINT]);
 
+  // const submitChat = (event: React.FormEvent) => {
+  //   event.preventDefault();
+  //   if (message && socket) {
+  //     socket.emit("submit_chat", { username, message });
+  //     setMessage("");
+  //   }
+  // };
+
   const submitChat = (event: React.FormEvent) => {
     event.preventDefault();
-    if (chat && socket) {
-      socket.emit("submit_chat", chat);
-      setChat("");
+    if (message && socket) {
+      const chatData = { username, message, socketId: socket.id };
+      socket.emit("submit_chat", chatData);
+      setChatList((oldChatList) => [...oldChatList, chatData]);
+      setMessage("");
     }
   };
 
-  const joinChat = (event: React.FormEvent) => {
-    event.preventDefault();
-    if (username && socket) {
-      socket.emit("new_user", username);
-    }
-  };
+  // const joinChat = (event: React.FormEvent) => {
+  //   event.preventDefault();
+  //   if (username && socket) {
+  //     socket.emit("new_user", username);
+  //   }
+  // };
 
   return (
     <div>
-      <form onSubmit={joinChat}>
-        <Input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Enter your username"
-        />
-        <Button type="submit">Join Chat</Button>
-      </form>
-
-      <form onSubmit={submitChat}>
-        <Input
-          type="text"
-          value={chat}
-          onChange={(e) => setChat(e.target.value)}
-          placeholder="Enter your message"
-        />
-        <Button type="submit">Send</Button>
-      </form>
-
-      {chatList.map((chatItem, index) => (
-        <div key={index}>
-          <h4>
-            {chatItem.username}: {chatItem.chat}
-          </h4>
-        </div>
-      ))}
+      <Input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Enter your username"
+      />
+      <Flex
+        flexDirection="column"
+        width="100%"
+        overflowY="auto"
+        minHeight="80vh"
+        maxHeight="85vh"
+      >
+        {chatList.map((chatItem, index) => {
+          const isCurrentUser = socket?.id === chatItem.socketId;
+          return (
+            <Box
+              key={index}
+              alignSelf={isCurrentUser ? "flex-end" : "flex-start"}
+              maxW="70%"
+              backgroundColor={isCurrentUser ? "teal.500" : "gray.300"}
+              borderRadius="md"
+              p={3}
+              mt={0.5}
+              textAlign={isCurrentUser ? "right" : "left"}
+            >
+              {isCurrentUser && (
+                <Text fontSize="md" color={"white"}>
+                  {chatItem.message}
+                </Text>
+              )}
+              {!isCurrentUser && (
+                <Text fontSize="md" color={"black"}>
+                  {chatItem.username} : {chatItem.message}
+                </Text>
+              )}
+            </Box>
+          );
+        })}
+        <div ref={messagesEndRef} />
+      </Flex>
+      <Box position="fixed" bottom="0" width="100%">
+        <form onSubmit={submitChat}>
+          <Flex flexDirection="row">
+            <Input
+              type="text"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              placeholder="Enter your message"
+            />
+            <Button type="submit">Send</Button>
+          </Flex>
+        </form>
+      </Box>
     </div>
   );
 };
