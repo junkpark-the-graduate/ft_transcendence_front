caf4287949d32b868840ef7ebea260e01d50bd86
Muser218 <aogidwndeld@naver.com>
2023-08-02 21:36:30 +0900

    feat: my profile DB connect
    Co-authored-by: junqwoopark <junqwoopark@users.noreply.github.com>

--- src/app/user/profile/page.tsx
+++ src/app/user/profile/page.tsx
@@ -3,12 +3,7 @@
 import Dashboard from "../../../ui/Dashboard/Dashboard";
 import GridType1 from "@/ui/Grid/GridType1";
 import MyDetail from "../../../ui/Dashboard/MyDetail";
-import { getMyData } from "@/utils/user/getMyData";
 
-export default function MyProfile() {
-  const myId: number | undefined = getMyData()?.id;
-
-  return (
-    <GridType1 children={<Dashboard userData={<MyDetail />} id={myId} />} />
-  );
+export default async function MyProfile() {
+  return <GridType1 children={<Dashboard userData={<MyDetail />} />} />;
 }
