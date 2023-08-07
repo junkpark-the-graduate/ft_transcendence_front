5bdffed575ff3fd88b92f3b80ce4eddf1e6954ff
namzisun <namzisun@gmail.com>
2023-08-07 21:18:07 +0900

    feat: dm message 버튼 연결 in user profile

--- src/ui/Dashboard/UserDetail.tsx
+++ src/ui/Dashboard/UserDetail.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { Avatar, Box, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import BaseButton from "@/ui/Button/Button";
 import FollowButton from "@/ui/Button/FollowButton";
 import { getMyData } from "@/utils/user/getMyData";
@@ -8,10 +16,46 @@ import { getUserData } from "@/utils/user/getUserData";
 import BlockButton from "@/ui/Button/BlockButton";
 import { EUserStatus } from "@/app/user/types/EUserStatus";
 import { GoComment } from "react-icons/go";
+import { useRouter } from "next/navigation";
+import Cookies from "js-cookie";
 
 export default function UserDetail({ userId }: { userId: number }) {
+  const accessToken = Cookies.get("accessToken");
   const myId: number | undefined = getMyData()?.id;
   const userData = getUserData(userId);
+  const router = useRouter();
+  const toast = useToast();
+
+  const createDm = async () => {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACK_END_POINT}/channel/direct?memberId=${userId}`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return res;
+  };
+
+  const handleConnectDm = async () => {
+    const resCreateDm = await createDm();
+    const resCreateDmJson = await resCreateDm.json();
+
+    if (resCreateDm.status < 300) {
+      router.push(`/channel/${resCreateDmJson.id}/chat-room`);
+    } else {
+      toast({
+        title: resCreateDmJson.message,
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
     <Box position="relative" px={5} pt={4} borderRadius={8} mb={2}>
@@ -42,7 +86,7 @@ export default function UserDetail({ userId }: { userId: number }) {
                 size="sm"
                 leftIcon={<GoComment />}
                 text="message"
-                onClick={() => {}}
+                onClick={handleConnectDm}
               />
               <BlockButton myId={myId} userId={userId} icon={true} />
             </Flex>
