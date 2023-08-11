c8c04672137c7656befa66e53247b39a7b518549
hermin9804 <hermin9804@gmail.com>
2023-08-11 14:26:23 +0900

    feat: directChannel.name 깜박거리는 문제 해결
    Co-authored-by: namzisun <namzisun@users.noreply.github.com>

--- src/ui/Lists/DmList.tsx
+++ src/ui/Lists/DmList.tsx
@@ -1,49 +1,18 @@
 "use client";
 
-import React, { use, useEffect, useState } from "react";
-import {
-  Box,
-  Flex,
-  Text,
-  useToast,
-  Avatar,
-  HStack,
-  Badge,
-  IconButton,
-} from "@chakra-ui/react";
-import { EChannelType } from "../../app/(chat)/channel/types/EChannelType";
+import React, { useEffect, useState } from "react";
+import { Box, Flex, Text, useToast, Avatar, HStack } from "@chakra-ui/react";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
-import PasswordModal from "@/ui/Modal/PasswordModal";
-import Input from "@/ui/Input/Input";
 import ButtonBox from "@/ui/Box/ButtonBox";
-import CreateChannelModal from "@/ui/Modal/CreateChannelModal";
 import { formatCreatedAt } from "@/utils/chat/formatCreatedAt";
 import ChannelBadge from "../Badges/ChannelBadge";
-import { GoSync } from "react-icons/go";
-import { set } from "react-hook-form";
-import { getMyData } from "@/utils/user/getMyData";
-import { getUserData } from "@/utils/user/getUserData";
-import { get } from "http";
 
 const DmList: React.FC = () => {
   const accessToken = Cookies.get("accessToken");
-  const [user, setUser] = useState<{ [key: string]: any }>({});
   const router = useRouter();
   const toast = useToast();
-  const [searchKeyword, setSearchKeyword] = useState<string>("");
   const [channels, setChannels] = useState<any[]>([]);
-
-  const getUser = async () => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACK_END_POINT}/user`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
-    return res.json();
-  };
 
   async function getDirectChannels() {
     const accessToken = Cookies.get("accessToken");
@@ -58,51 +27,15 @@ const DmList: React.FC = () => {
       }
     );
     const resJson = await res.json();
-    setChannels(resJson);
-    return resJson;
-  }
-
-  async function getUserDataById(userId: number) {
-    const accessToken = Cookies.get("accessToken");
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACK_END_POINT}/user/${userId}`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }
-    );
-    const resJson = await res.json();
     return resJson;
   }
 
   useEffect(() => {
     if (!accessToken) router.push("/");
-    getUser().then((res) => {
-      setUser(res);
+    getDirectChannels().then((res) => {
+      setChannels(res);
     });
   }, []);
-
-  useEffect(() => {
-    if (!user) return;
-    getDirectChannels().then(async (res) => {
-      const updatedChannels = await Promise.all(
-        res.map(async (channel: any) => {
-          const userIds: number[] = channel.name
-            .split("-")
-            .map((id: string) => parseInt(id, 10)); // 문자열을 숫자로 변환
-          const directChannelUserId = userIds.find((id) => id !== user.id);
-          if (!directChannelUserId) return channel;
-          const directChannelUser = await getUserDataById(directChannelUserId);
-          channel.name = directChannelUser?.name;
-          return channel;
-        })
-      );
-      setChannels(updatedChannels);
-    });
-  }, [user]);
 
   async function connectJoinedChannel(channelId: number) {
     const accessToken = Cookies.get("accessToken");
@@ -135,34 +68,10 @@ const DmList: React.FC = () => {
     }
   }
 
-  async function syncChannelsHandler() {
-    getDirectChannels();
-  }
-
   return (
     <>
       <Box px={6} py={4}>
-        <Box mt={2}>
-          <Flex direction="row" gap={3} alignItems="center" mb={8}>
-            <Input
-              type="text"
-              placeholder="채널 이름 검색"
-              value={searchKeyword}
-              onChange={() => console.log("검색어 입력")}
-            />
-            <IconButton
-              aria-label="채널 목록 새로고침"
-              icon={<GoSync />}
-              bg="#414147"
-              borderRadius={"8px"}
-              textColor="white"
-              _hover={{
-                background: "#191919",
-              }}
-              onClick={syncChannelsHandler}
-            />
-          </Flex>
-        </Box>
+        <Box mt={2}></Box>
         <Flex direction="column" gap={3}>
           {channels.map((channel: any) => {
             return (
